614fcf486c205963e6733ccfe14670c7e10beda5
jgozner <jgozner@outlook.com>
2024-10-31 09:29:32 -0700

    Update config_apex.js

--- force-app/main/default/staticresources/myfiles/config_apex.js
+++ force-app/main/default/staticresources/myfiles/config_apex.js
@@ -43,7 +43,7 @@ async function saveDocument() {
   if (!doc) {
     return;
   }
-  instance.openElement('loadingModal');
+  instance.UI.openElement('loadingModal');
   const fileSize = await doc.getFileSize();
   const fileType = doc.getType();
   const filename = doc.getFilename();
@@ -111,7 +111,7 @@ window.addEventListener('viewerLoaded', async function () {
     var myCustomButton = {
       type: 'actionButton',
       dataElement: 'saveDocumentButton',
-      title: 'tool.SaveDocument',
+      title: 'Save',
       img: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>',
       onClick: function () {
         saveDocument();
@@ -143,7 +143,7 @@ function receiveMessage(event) {
         break;
       case 'DOCUMENT_SAVED':
         console.log(`${JSON.stringify(event.data)}`);
-        instance.showErrorMessage('Document saved ')
+        window.alert("Document saved to salesforce")
         setTimeout(() => {
           instance.UI.closeElements(['errorModal', 'loadingModal'])
         }, 2000)
